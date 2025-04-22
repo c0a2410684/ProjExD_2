b50a2e108a4f0df4977907a6992424ee08e21043
c0a2410684 <c0a2410684@edu.teu.ac.jp>
2025-04-22 17:30:55 +0900

    演習課題追加機能３途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -46,14 +46,52 @@ def main():
     tmr = 0
     
     def gameover(screen: pg.Surface) -> None:
+        """
+        ゲームオーバー時に，半透明の黒い画面上に「Game Over」と表
+示し，泣いているこうかとん画像を貼り付ける関数
+        """
+
+        height: int = 280  #こうかとんと文字の高さ
         screen.blit(bl_img, bl_rct)
-        screen.blit(txt, [400, 300])
-        screen.blit(kokaton, [330, 280])
-        screen.blit(kokaton, [730, 280])
+        screen.blit(txt, [400, height])
+        screen.blit(kokaton, [330, height])
+        screen.blit(kokaton, [730, height])
 
         pg.display.update()
         time.sleep(5)
     
+    # bb_accs = [a for a in range(1, 11)]
+    # for r in range(1, 11):
+    #     bb_img = pg.surface((20*r, 20*r))
+    #     pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+
+    # def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    #     bb_accs = tuple(bb_accs)
+    #     bb_img = tuple(bb_img)
+        
+    kk_img2 = pg.transform.flip(kk_img, True, False)
+    def get_kk_img(sum_mv: tuple[int, int]) -> pg.Surface:
+        jisyo = {
+            (-5,0):pg.transform.rotozoom(kk_img, 0, 1.0),
+            (-5,-5):pg.transform.rotozoom(kk_img, -45, 1.0),
+            (-5,5):pg.transform.rotozoom(kk_img, 45, 1.0),
+            (0,-5):pg.transform.rotozoom(kk_img2, 90, 1.0),
+            (5,-5):pg.transform.rotozoom(kk_img2, 45, 1.0),
+            (5,0):pg.transform.rotozoom(kk_img2, 0, 1.0),
+            (5,5):pg.transform.rotozoom(kk_img2, -45, 1.0),
+            (0,5):pg.transform.rotozoom(kk_img2, -90, 1.0),
+        }
+        for k, v in jisyo.items():
+            if sum_mv == k:
+                return v
+        else:
+            return pg.transform.rotozoom(kk_img, 0, 1.0)
+            
+        
+        
+
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -61,7 +99,8 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         if kk_rct.colliderect(bb_rct):
-            gameover(screen)
+            gameover(screen)  #関数の実装
+            print(gameover.__doc__)  #docstring
             print("game over")
             return
         
@@ -73,19 +112,22 @@ def main():
                 sum_mv[0] += x
                 sum_mv[1] += y
 
-        
+        # bb_imgs, bb_accs = init_bb_imgs()
+        # avx = vx*bb_accs[min(tmr//500, 9)]
+        # bb_img = bb_imgs[min(tmr//500, 9)]
+        kk_imgs = get_kk_img(sum_mv)
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True, True):
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy)
+        if check_bound(kk_rct) != (True, True): #画面外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) #画面内に戻す
+        screen.blit(kk_imgs, kk_rct)
+        bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
         if not yoko:  # 左右どちらかにはみ出ていたら
             vx *= -1
         if not tate:  # 上下どちらかにはみ出ていたら
             vy *= -1
 
-        screen.blit(bb_img, bb_rct)
+        screen.blit(bb_img, bb_rct)  #爆弾の描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
